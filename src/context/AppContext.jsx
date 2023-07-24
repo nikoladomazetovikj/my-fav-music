939f636c970526078bd6c65e295440245b72586d
nikoladomazetovikj <nikolad150@gmail.com>
2023-07-24 14:50:30 +0200

    wip: search implementation

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,16 +1,60 @@
-import { createContext } from "react";
+import {createContext, useEffect, useState} from "react";
+import axios from "axios";
 
-const AppContext = createContext();
+const AppContext = createContext({
+    searchResults: []
+});
 
-const AppProvider = ({ children }) => {
-    // TODO: Define all initial states here
-    const initialStates = {};
+// eslint-disable-next-line react/prop-types
+export const AppProvider = ({ children }) => {
+
+    console.log('in')
+
+    const [searchResults, setSearchResults] = useState([]);
+
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const options = {
+                method: 'GET',
+                url: `${process.env.REACT_APP_BASE_URL}/search`,
+                params: {
+                    term: 'maks korzh',
+                    locale: 'en-US',
+                    offset: '0',
+                    limit: '5',
+                },
+                headers: {
+                    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_KEY,
+                    'X-RapidAPI-Host': process.env.REACT_APP_RAPID_HOST,
+                },
+            };
+
+            try {
+                const response = await axios.request(options);
+                setSearchResults(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+
+
+        fetchData();
+    }, []);
+
+    const contextValues = {
+       searchResults
+    };
+
+
 
     return (
-        <AppContext.Provider value={initialStates}>
+        <AppContext.Provider value={contextValues}>
             {children}
         </AppContext.Provider>
     );
 };
 
-export default AppProvider;
+export default AppContext;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import Header from "../components/Layout/Header.jsx";
+import {useContext} from "react";
+import AppContext from "../context/AppContext.jsx";
 
 const Home = () => {
 
+    const {searchResults} = useContext(AppContext);
+
+    console.log(searchResults)
     return (
         <>
             <Header/>
