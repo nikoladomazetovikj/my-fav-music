1d46dff69c048f289fb1a134efdabb53445b781c
nikoladomazetovikj <nikolad150@gmail.com>
2023-07-24 13:44:48 +0200

    feat: page not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,4 +1,17 @@
-const NotFound = () => {}
+import {Container, Row} from "react-bootstrap";
+
+const NotFound = () => {
+
+    return (
+        <Container className='my-5'>
+            <Row className='text-center'>
+                <h1>404 Error</h1>
+                <hr/>
+                <h3>Page not Found</h3>
+            </Row>
+        </Container>
+    );
+}
 
 
 export default NotFound;
